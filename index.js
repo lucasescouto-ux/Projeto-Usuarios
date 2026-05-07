383c0e9cabe31c4a9959a36b22a5d01497961211
Lucas Silva <lucasescouto05@gmail.com>
2026-05-07 01:23:44 -0300

    Aula #30. G2 a #42. G14 - Adicionado funcionalidade de inserção de imagens.
    Funcionalidade de ''submit'' para envio da criação do usuário
    
    Funcionalidade de gênero masculino ou feminino, formando os seletores.

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 alert("Olá, seja bem-vindo ao sistema!");
+
+let userController = new UserController('form-user-create', 'table-users')
---
+++ models/users.js
@@ -0,0 +1,15 @@
+class User {
+
+    constructor(name, gender, bitrh, country, email, password, photo, admin){
+
+        this.name = name;
+        this.gender = gender;
+        this.bitrh = bitrh;
+        this.country = country;
+        this.email = email;
+        this.password = password;
+        this.photo = photo;
+        this.admin = admin;  
+
+    }
+}
